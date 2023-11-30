c17348cd1585981bba49cb190611c10f281b1486
Welley <monteirowelley7@gmail.com>
2023-11-30 10:00:00 -0300

    projeto da calculadora concluido

--- calc.js
+++ calc.js
@@ -1,5 +1,5 @@
 const main = document.querySelector('main')
-const root = document.querySelector('root')
+const root = document.querySelector(':root')
 const input = document.getElementById('input')
 const inputResult = document.getElementById('result')
 
@@ -22,7 +22,7 @@ document.querySelectorAll('.charKey').forEach((charKeyBtn) => {
 
 // Eventos de quando aperta na tecla
 
-input.addEventListener('keydown', function(ev) {
+input.addEventListener('keydown', function (ev) {
     ev.preventDefault()
     if (allowedKeys.includes(ev.key)) {
         input.value += ev.key
@@ -40,10 +40,40 @@ document.getElementById('equal').addEventListener('click', calculate)
 
 
 function calculate() {
+    resultInput.value = "ERROR"
+    resultInput.classList.add("error")
     const result = eval(input.value)
-    inputResult.value = result
+    resultInput.value = result
+    resultInput.classList.remove("error")
 }
 
+document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
+    const button = ev.currentTarget
+    if (button.innerText === "Copy") {
+        button.innerText = "Copied!"
+        button.classList.add("success")
+        navigator.clipboard.writeText(resultInput.value)
+    } else {
+        button.innerText = "Copy"
+        button.classList.remove("success")
+    }
+})
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+    if (main.dataset.theme === "dark") {
+        root.style.setProperty("--bg-color", "#f1f5f9")
+        root.style.setProperty("--border-color", "#aaa")
+        root.style.setProperty("--font-color", "#212529")
+        root.style.setProperty("--primary-color", "#26834a")
+        main.dataset.theme = "light"
+    } else {
+        root.style.setProperty("--bg-color", "#212529")
+        root.style.setProperty("--border-color", "#666")
+        root.style.setProperty("--font-color", "#f1f5f9")
+        root.style.setProperty("--primary-color", "#4dff91")
+        main.dataset.theme = "dark"
+    }
+})
 
 
 
